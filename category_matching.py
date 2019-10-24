a9ea77e6b0699e07fa3ca8484959b2af0b568762
Alt3rius <mikolajkula1994@gmail.com>
2019-10-24 12:01:53 +0200

    First tests

--- category_matching.py
+++ category_matching.py
@@ -5,5 +5,29 @@ from gensim.models import KeyedVectors
 
 model = KeyedVectors.load_word2vec_format("nkjp+wiki-forms-all-300-skipg-ns.txt")
 
-model.wmdistance("dom ogród", "dom ogródek")
+
+model.wmdistance("AGD Lodówki i zamrażarki Lodówki z zamrażalnikiem", "AGD Lodówki i zamrażarki Lodówki")
+#0.7890250358051902
+
+model.wmdistance("AGD Lodówki i zamrażarki Lodówki z zamrażalnikiem", "AGD Kuchenki Mikrofalowe")
+#2.246459659948586
+
+
+
+model.wmdistance("AGD Lodówki i zamrażarki Lodówki z zamrażalnikiem", "AGD Lodówki i zamrażarki Lodówki turystyczne")
+#1.1760916959489012
+
+
+model.wmdistance("AGD Lodówki i zamrażarki Lodówki z zamrażalnikiem", "SDKJHBDFGKLUDRGBKLJDJFBGLBJH")
+#4.176374081454891
+
+
+model.wmdistance("AGD Lodówki i zamrażarki Lodówki z zamrażalnikiem", "1432874568796452")
+#4.363080851045773
+
+
+
+model.wmdistance("AGD Lodówki i zamrażarki Lodówki z zamrażalnikiem", "AGD Lodówki i zamrażarki Lodówki do zabudowy")
+#1.1486862892547698
+
 
